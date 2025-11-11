053401c75e231b844c0bbe3ff267033364318352
cromka <arkadiuszkroma@gmail.com>
2025-11-11 18:49:51 +0100

    zrobiłem menu początkowe i jedyne co musimy zrobic to wypełnić funkcje

--- Pensjonat/Pensjonat.cpp
+++ Pensjonat/Pensjonat.cpp
@@ -1,20 +1,75 @@
-﻿// Pensjonat.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
+﻿
 #include <iostream>
-
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+class Pensjonat {
+public:
+	void wyswietlinfo() {
+		int numer;
+		cout << "Witamy w pensjonacie! Wpisz numer jednej z podanych opcji." << endl << endl;
+		cout << "1. Wyświetl dostępne pokoje" << endl;
+		cout << "2. Zarezerwuj jeden z pokoi." << endl;
+		cout << "3. Wyświetl zajęte pokoje." << endl;
+		cout << "4. Usuń rezerwację." << endl;
+		cout << "5. Wyświetl opisy pokoi" << endl;
+		cout << "6. Zamknij stronę pensjonatu." << endl;
+		cin >> numer;
+		if (numer == 1) {
+			wyswietldostepnepokoje();
+		}
+		else if (numer == 2) {
+			zarezerwujjedenzpokoi();
+		}
+		else if (numer == 3) {
+			wyswietlzajetepokoje();
+		}
+		else if (numer == 4) {
+			usunrezerwacje();
+		}
+		else if (numer == 5) {
+			wyswietlopisypokoi();
+		}
+		else if (numer == 6) {
+			koniec();
+		}
+		else {
+			cout << "Podałeś błędną wartość! Wpisz poprawną liczbę od 1-6" << endl;
+			wyswietlinfo();
+		}
+	}
+private:
+	void wyswietldostepnepokoje() {
+		cout << "dzialam1" << endl;
+		wyswietlinfo();
+	}
+	void zarezerwujjedenzpokoi() {
+		cout << "dzialam2" << endl;
+		wyswietlinfo();
+	}
+	void wyswietlzajetepokoje() {
+		cout << "dzialam3" << endl;
+		wyswietlinfo();
+	}
+	void usunrezerwacje() {
+		cout << "dzialam4" << endl;
+		wyswietlinfo();
+	}
+	void wyswietlopisypokoi() {
+		cout << "dzialam5" << endl;
+		wyswietlinfo();
+	}
+	int koniec() {
+		exit(0);
+	}
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+	Pensjonat obiekt1;
+	obiekt1.wyswietlinfo();
+	return 0;
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
