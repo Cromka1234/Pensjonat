d0c43b7a326d7dce8631a20abdd8b05c2e5dc665
Cromka1234 <arekkroma@gmail.com>
2025-11-24 14:35:30 +0100

    zrobiłem swoją część i się muszę nauczyć kodu
    Added functionality to add and remove rooms with descriptions in the hotel management system.

--- Pensjonat/Pensjonat.cpp
+++ Pensjonat/Pensjonat.cpp
@@ -5,6 +5,7 @@
 #include <windows.h>
 #include <locale>
 #include <codecvt>
+#include <vector>
 using namespace std;
 
 class Pensjonat {
@@ -351,12 +352,313 @@ private:
     }
 
     void dodawaniepokoju() {
-        cout << "dzialam6" << endl;
+        cin.clear();
+        cin.ignore(10000, '\n'); 
+
+        int maxnum = 0;
+        ifstream fin("dostepnepokoje.txt");
+        if (fin.is_open()) {
+            string linia;
+            while (getline(fin, linia)) {
+                if (linia == "") continue;
+                int j = 0;
+                int len = (int)linia.length();
+                // znajdź pierwszą cyfrę
+                while (j < len && !(linia[j] >= '0' && linia[j] <= '9')) j++;
+                int start = j;
+                while (j < len && (linia[j] >= '0' && linia[j] <= '9')) j++;
+                if (start < j) {
+                    string numstr = linia.substr(start, j - start);
+                    int num = 0;
+                    try {
+                        num = stoi(numstr);
+                    } catch (...) {
+                        num = 0;
+                    }
+                    if (num > maxnum) maxnum = num;
+                }
+            }
+            fin.close();
+        }
+
+        int nowyNr = maxnum + 1;
+
+        cout << "Podaj nazwę pokoju: ";
+        string nazwa;
+        getline(cin, nazwa);
+        if (nazwa == "") {
+            cout << "Nie podano nazwy. Anulowano dodawanie." << endl;
+            wyswietlinfo();
+            return;
+        }
+
+        cout << "Podaj cenę (liczba, bez PLN): ";
+        string cena;
+        getline(cin, cena);
+        if (cena == "") {
+            cout << "Nie podano ceny. Anulowano dodawanie." << endl;
+            wyswietlinfo();
+            return;
+        }
+
+        cout << "Podaj opis pokoju. Zakończ wpis linią zawierającą tylko kropkę '.'" << endl;
+        string opisLinia;
+        vector<string> opisWiersze;
+        while (true) {
+            if (!getline(cin, opisLinia)) break;
+            if (opisLinia == ".") break;
+            opisWiersze.push_back(opisLinia);
+        }
+        if ((int)opisWiersze.size() == 0) {
+            cout << "Nie podano opisu. Anulowano dodawanie." << endl;
+            wyswietlinfo();
+            return;
+        }
+
+        ofstream out("dostepnepokoje.txt", ios::app);
+        if (!out) {
+            cout << "Błąd otwarcia pliku dostepnepokoje.txt" << endl;
+            wyswietlinfo();
+            return;
+        }
+        out << nowyNr << ". " << nazwa << " - " << cena << " PLN" << '\n';
+        out.close();
+
+        bool opisPusty = true;
+        ifstream testopis("opisypokoi.txt", ios::binary | ios::ate);
+        if (testopis) {
+            if (testopis.tellg() > 0) opisPusty = false;
+            testopis.close();
+        }
+        ofstream outOpis("opisypokoi.txt", ios::app);
+        if (!outOpis) {
+            cout << "Błąd otwarcia pliku opisypokoi.txt" << endl;
+            wyswietlinfo();
+            return;
+        }
+        if (!opisPusty) outOpis << '\n';
+        outOpis << nowyNr << ". " << opisWiersze[0] << '\n';
+        int idx = 1;
+        int total = (int)opisWiersze.size();
+        while (idx < total) {
+            outOpis << opisWiersze[idx] << '\n';
+            idx = idx + 1;
+        }
+        outOpis << '\n';
+        outOpis.close();
+
+        cout << "Dodano pokój nr " << nowyNr << " wraz z opisem." << endl;
         wyswietlinfo();
     }
 
     void usuwaniepkoju() {
-        cout << "dzialam7" << endl;
+        vector<string> dost;
+        string linia;
+        ifstream fin("dostepnepokoje.txt");
+        if (fin.is_open()) {
+            while (getline(fin, linia)) {
+                if (linia != "") dost.push_back(linia);
+            }
+            fin.close();
+        }
+        cout << "Dostępne pokoje:" << endl;
+        int i = 0;
+        int ileDost = (int)dost.size();
+        while (i < ileDost) {
+            cout << (i + 1) << ". " << dost[i] << endl;
+            i = i + 1;
+        }
+
+        vector<string> zaj;
+        ifstream fin2("zajetepokoje.txt");
+        if (fin2.is_open()) {
+            while (getline(fin2, linia)) {
+                if (linia != "") zaj.push_back(linia);
+            }
+            fin2.close();
+        }
+        cout << "Zajęte pokoje:" << endl;
+        i = 0;
+        int ileZaj = (int)zaj.size();
+        while (i < ileZaj) {
+            cout << (i + 1) << ". " << zaj[i] << endl;
+            i = i + 1;
+        }
+
+        if (ileDost == 0 && ileZaj == 0) {
+            cout << "Brak pokoi do usunięcia." << endl;
+            wyswietlinfo();
+            return;
+        }
+
+        cout << "Usuń z (d)ostępne czy (z)ajęte? ";
+        char wybor = ' ';
+        cin >> wybor;
+        cout << endl;
+        if (!(wybor == 'd' || wybor == 'D' || wybor == 'z' || wybor == 'Z')) {
+            cout << "Nieprawidłowy wybór." << endl;
+            wyswietlinfo();
+            return;
+        }
+
+        cout << "Podaj numer pokoju do usunięcia: ";
+        int numer = 0;
+        if (!(cin >> numer)) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Nieprawidłowy numer." << endl;
+            wyswietlinfo();
+            return;
+        }
+
+        string usunietaLinia = "";
+        if (wybor == 'd' || wybor == 'D') {
+            if (numer < 1 || numer > ileDost) {
+                cout << "Numer poza zakresem listy dostępnych." << endl;
+                wyswietlinfo();
+                return;
+            }
+            int idx = 0;
+            while (idx < ileDost) {
+                if (idx == (numer - 1)) {
+                    usunietaLinia = dost[idx];
+                }
+                idx = idx + 1;
+            }
+            ofstream out("dostepnepokoje.txt", ios::trunc);
+            if (!out) {
+                cout << "Błąd zapisu pliku dostepnepokoje.txt" << endl;
+                wyswietlinfo();
+                return;
+            }
+            idx = 0;
+            while (idx < ileDost) {
+                if (idx != (numer - 1)) {
+                    out << dost[idx] << '\n';
+                }
+                idx = idx + 1;
+            }
+            out.close();
+        } else {
+            if (numer < 1 || numer > ileZaj) {
+                cout << "Numer poza zakresem listy zajętych." << endl;
+                wyswietlinfo();
+                return;
+            }
+            int idx = 0;
+            while (idx < ileZaj) {
+                if (idx == (numer - 1)) {
+                    usunietaLinia = zaj[idx];
+                }
+                idx = idx + 1;
+            }
+            ofstream out("zajetepokoje.txt", ios::trunc);
+            if (!out) {
+                cout << "Błąd zapisu pliku zajetepokoje.txt" << endl;
+                wyswietlinfo();
+                return;
+            }
+            idx = 0;
+            while (idx < ileZaj) {
+                if (idx != (numer - 1)) {
+                    out << zaj[idx] << '\n';
+                }
+                idx = idx + 1;
+            }
+            out.close();
+        }
+
+        string numstr = "";
+        int j = 0;
+        int dllen = (int)usunietaLinia.length();
+        while (j < dllen && !(usunietaLinia[j] >= '0' && usunietaLinia[j] <= '9')) j++;
+        int start = j;
+        while (j < dllen && (usunietaLinia[j] >= '0' && usunietaLinia[j] <= '9')) j++;
+        if (start < j) {
+            numstr = usunietaLinia.substr(start, j - start);
+        }
+
+        if (numstr == "") {
+            cout << "Nie udało się odnaleźć numeru pokoju do usunięcia opisu." << endl;
+            wyswietlinfo();
+            return;
+        }
+
+        vector<string> all;
+        ifstream finOpis("opisypokoi.txt");
+        if (finOpis.is_open()) {
+            while (getline(finOpis, linia)) {
+                all.push_back(linia);
+            }
+            finOpis.close();
+        }
+
+        int total = (int)all.size();
+        int startLine = -1;
+        int p = 0;
+        while (p < total) {
+            string l = all[p];
+            int k = 0;
+            int llen = (int)l.length();
+            while (k < llen && l[k] == ' ') k++;
+            int needLen = (int)numstr.length();
+            if (k + needLen < llen) {
+                bool match = true;
+                int t = 0;
+                while (t < needLen) {
+                    if (k + t >= llen || l[k + t] != numstr[t]) { match = false; break; }
+                    t = t + 1;
+                }
+                if (match && k + needLen < llen && l[k + needLen] == '.') {
+                    startLine = p;
+                    break;
+                }
+            }
+            p = p + 1;
+        }
+
+        if (startLine != -1) {
+            int q = startLine;
+            while (q < total) {
+                if (all[q] == "") {
+                    break;
+                }
+                q = q + 1;
+            }
+            vector<string> nowy;
+            int r = 0;
+            while (r < startLine) {
+                nowy.push_back(all[r]);
+                r = r + 1;
+            }
+            if (q < total) {
+                r = q + 1;
+            } else {
+                r = q;
+            }
+            while (r < total) {
+                nowy.push_back(all[r]);
+                r = r + 1;
+            }
+            ofstream outOpis("opisypokoi.txt", ios::trunc);
+            if (outOpis.is_open()) {
+                int s = 0;
+                int nTotal = (int)nowy.size();
+                while (s < nTotal) {
+                    outOpis << nowy[s] << '\n';
+                    s = s + 1;
+                }
+                outOpis.close();
+            } else {
+                cout << "Błąd zapisu pliku opisypokoi.txt" << endl;
+                wyswietlinfo();
+                return;
+            }
+        } else {
+            cout << "Nie znaleziono opisu pokoju o numerze " << numstr << " w pliku opisypokoi.txt" << endl;
+        }
+
+        cout << "Usunięto pokój i (jeśli znaleziono) jego opis. Numer: " << numstr << endl;
         wyswietlinfo();
     }
     void wyswietlpokojevip() {
